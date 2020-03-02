3729d83ae14c334a39514ca728d3a6cb99894b81
Dan1eLx <d4n1elx3@gmail.com>
2020-03-02 23:46:46 +0500

    header

--- src/blocks/components/header/header.js
+++ src/blocks/components/header/header.js
@@ -1,13 +1,33 @@
-modules.define('header', ['i-bem-dom'], function(provide, bemDom) {
-
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
-
+$(document).ready(function () {
+    initOutHeader();
+    initActiveMenu();
 });
+function initOutHeader() {
+    var header = $('.header'),
+        scrollPrev = 0;
+    if (header.length){
+        $(window).scroll(function () {
+            var scrolled = $(window).scrollTop();
+
+            if (scrolled > 100 && scrolled > scrollPrev) {
+                header.addClass('header_out');
+            } else {
+                header.removeClass('header_out');
+            }
+            scrollPrev = scrolled;
+        });
+    }
+}
+
+function initActiveMenu() {
+    $('#menu-button').off('click');
+    $('#menu-button').on('click', function () {
+        $(this).toggleClass('menu-open');
+        $('body').toggleClass('overflow-hidden');
+        if ($('.header').not('.header_scrolled')){
+            $('.header').addClass('header_scrolled');
+        }
+        $('.menu-wrapper').toggleClass('menu-wrapper_open')
+    });
+
+}
